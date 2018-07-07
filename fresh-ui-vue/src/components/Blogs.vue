891973bb2923be9f6774bc46a4333e8d41ae3be8
Manuel RojasL <manuel@seesawlabs.com>
2018-07-07 07:29:21 -0600

    Initial Vue App :)

---
+++ fresh-ui-vue/src/components/Blogs.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Latest News</h1>
+
+    <ol>
+      <li v-bind:key="blog.id" v-for="blog in blogs">
+        {{ blog.title }}
+        <p>{{ blog.content }}</p>
+      </li>
+    </ol>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Blogs',
+  data:function() {
+    return  {
+      blogs:
+        [{
+
+          id: 1,
+          title: "First Blog",
+          content: "About Vue"
+        },
+        {
+          id: 2,
+          title: "First Blog",
+          content: "About Vue"
+        },
+        {
+          id: 3,
+          title: "First Blog",
+          content: "About Vue"
+        }]
+    }
+  }
+}
+</script>
